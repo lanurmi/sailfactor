96dc40b3f5e9b79803d21270ce3fea8fcb005028
Lauri Nurmi <lanurmi@iki.fi>
2014-03-05 01:34:22 +0200

    Add Spanish translation.

---
+++ lang/sailfactor_es.ts
@@ -0,0 +1,81 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../src/qml/pages/AboutPage.qml" line="21"/>
+        <source>About Sailfactor %1</source>
+        <translation>Acerca de Sailfactor %1</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/AboutPage.qml" line="25"/>
+        <source>License</source>
+        <translation>Licencia</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/AboutPage.qml" line="42"/>
+        <source>Author</source>
+        <translation>Autor</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/AboutPage.qml" line="55"/>
+        <source>Source code</source>
+        <translation>Código fuente</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/AboutPage.qml" line="64"/>
+        <source>available at GitHub</source>
+        <translation>disponible en GitHub</translation>
+    </message>
+</context>
+<context>
+    <name>HelpPage</name>
+    <message>
+        <location filename="../src/qml/pages/HelpPage.qml" line="12"/>
+        <source>Sailfactor Help</source>
+        <translation>Ayuda de Sailfactor</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/HelpPage.qml" line="21"/>
+        <source>Sailfactor is a tool for finding the factors of an integer, step by step. The slider is used to adjust the maximum number of factorization steps to carry out.&lt;br&gt;&lt;br&gt;&lt;i&gt;Prime&lt;/i&gt; factors are always &lt;b&gt;highlighted&lt;/b&gt; in the output. When the number of steps is high enough, all the factors shown will be prime factors.</source>
+        <translation>Sailfactor es una herramienta para encontrar los factores de los números enteros, paso a paso. Se usa el deslizador para ajustar número de pasos que llevan a cabo.&lt;br&gt;&lt;br&gt;Factores &lt;i&gt;primos&lt;/i&gt; siempre están &lt;b&gt;resaltados&lt;/b&gt; en la salida. Con bastantes pasos, todos los factores mostrados estarán factores primos.</translation>
+    </message>
+</context>
+<context>
+    <name>LicensePage</name>
+    <message>
+        <location filename="../src/qml/pages/LicensePage.qml" line="21"/>
+        <source>License for Sailfactor</source>
+        <translation>Licencia de Sailfactor</translation>
+    </message>
+</context>
+<context>
+    <name>SailFactor</name>
+    <message>
+        <location filename="../src/qml/pages/SailFactor.qml" line="15"/>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/SailFactor.qml" line="22"/>
+        <source>Help</source>
+        <translation>Ayuda</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/SailFactor.qml" line="38"/>
+        <source>Factorization</source>
+        <translation>Factorización</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/SailFactor.qml" line="54"/>
+        <source>Enter an integer.</source>
+        <translation>Escriba un número entero.</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/SailFactor.qml" line="83"/>
+        <source>Steps</source>
+        <translation>Pasos</translation>
+    </message>
+</context>
+</TS>
