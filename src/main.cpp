676c751ed7e767f8bc7f6fb28477d8ba338ee01d
Lauri Nurmi <lanurmi@iki.fi>
2014-03-05 00:40:57 +0200

    Remove comments originating from Helloworld Pro...

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,6 @@
 
 int main(int argc, char *argv[])
 {
-    // For this example, wizard-generates single line code would be good enough,
-    // but very soon it won't be enough for you anyway, so use this more detailed example from start
     QScopedPointer<QGuiApplication> app(SailfishApp::application(argc, argv));
     QScopedPointer<QQuickView> view(SailfishApp::createView());
 
@@ -21,9 +19,6 @@ int main(int argc, char *argv[])
     runUnitTests();
 #endif
 
-//    Here's how you will add QML components whenever you start using them
-//    Check https://github.com/amarchen/Wikipedia for a more full example
-//    view->engine()->addImportPath(SailfishApp::pathTo("qml/components").toString());
     view->engine()->rootContext()->setContextProperty("fact", &fact);
     view->setSource(SailfishApp::pathTo("qml/main.qml"));
 
