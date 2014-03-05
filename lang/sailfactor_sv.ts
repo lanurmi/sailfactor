b54319b5d2ff03f8312554ac986b95307ebd10b6
Lauri Nurmi <lanurmi@iki.fi>
2014-03-05 23:53:41 +0200

    Add Swedish translation.

---
+++ lang/sailfactor_sv.ts
@@ -0,0 +1,81 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="sv_SE">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../src/qml/pages/AboutPage.qml" line="21"/>
+        <source>About Sailfactor %1</source>
+        <translation>Om Sailfactor %1</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/AboutPage.qml" line="25"/>
+        <source>License</source>
+        <translation>Licens</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/AboutPage.qml" line="42"/>
+        <source>Author</source>
+        <translation>Utvecklare</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/AboutPage.qml" line="55"/>
+        <source>Source code</source>
+        <translation>Källkod</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/AboutPage.qml" line="64"/>
+        <source>available at GitHub</source>
+        <translation>tillgänglig på GitHub</translation>
+    </message>
+</context>
+<context>
+    <name>HelpPage</name>
+    <message>
+        <location filename="../src/qml/pages/HelpPage.qml" line="12"/>
+        <source>Sailfactor Help</source>
+        <translation>Hjälp för Sailfactor</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/HelpPage.qml" line="21"/>
+        <source>Sailfactor is a tool for finding the factors of an integer, step by step. The slider is used to adjust the maximum number of factorization steps to carry out.&lt;br&gt;&lt;br&gt;&lt;i&gt;Prime&lt;/i&gt; factors are always &lt;b&gt;highlighted&lt;/b&gt; in the output. When the number of steps is high enough, all the factors shown will be prime factors.</source>
+        <translation>Sailfactor är ett verktyg som används för att söka faktorer av ett heltal, steg för steg. Med skjutreglaget regleras antal faktoriseringssteg som utförs.&lt;br&gt;&lt;br&gt;&lt;i&gt;Prim&lt;/i&gt;faktorer visas alltid &lt;b&gt;markerade&lt;/b&gt;. När antal steg är tillräckligt hög, så skall alla faktorer som visas vara primfaktorer.</translation>
+    </message>
+</context>
+<context>
+    <name>LicensePage</name>
+    <message>
+        <location filename="../src/qml/pages/LicensePage.qml" line="21"/>
+        <source>License for Sailfactor</source>
+        <translation>Licens för Sailfactor</translation>
+    </message>
+</context>
+<context>
+    <name>SailFactor</name>
+    <message>
+        <location filename="../src/qml/pages/SailFactor.qml" line="15"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/SailFactor.qml" line="22"/>
+        <source>Help</source>
+        <translation>Hjälp</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/SailFactor.qml" line="38"/>
+        <source>Factorization</source>
+        <translation>Faktorisering</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/SailFactor.qml" line="54"/>
+        <source>Enter an integer.</source>
+        <translation>Ange ett heltal.</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/SailFactor.qml" line="83"/>
+        <source>Steps</source>
+        <translation>Steg</translation>
+    </message>
+</context>
+</TS>
