7792e4d51f1423dcd2aa43432d8acaae587a4743
Lauri Nurmi <lanurmi@iki.fi>
2014-07-03 16:57:10 +0300

    Show usage information if no arguments given.

--- translationupdate.sh
+++ translationupdate.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # This script should be run on the MerSDK virtual machine.
 
+if [ "$1" == "" ]; then
+    echo "Usage: $0 <2-letter-langcode>"
+    exit 1
+fi
+
 /srv/mer/targets/SailfishOS-i486-x86/usr/lib/qt5/bin/lupdate src/qml/{,cover,pages}/*.qml -verbose -ts lang/sailfactor_$1.ts 
 /srv/mer/targets/SailfishOS-i486-x86/usr/lib/qt5/bin/lrelease lang/sailfactor_$1.ts 
