66dd6ac652452d4911ee46dda5fdef95a72e119a
Lauri Nurmi <lanurmi@iki.fi>
2014-07-03 16:59:42 +0300

    Fix lupdate/lrelease paths to match the newest SDK paths.

--- translationupdate.sh
+++ translationupdate.sh
@@ -6,5 +6,5 @@ if [ "$1" == "" ]; then
     exit 1
 fi
 
-/srv/mer/targets/SailfishOS-i486-x86/usr/lib/qt5/bin/lupdate src/qml/{,cover,pages}/*.qml -verbose -ts lang/sailfactor_$1.ts 
-/srv/mer/targets/SailfishOS-i486-x86/usr/lib/qt5/bin/lrelease lang/sailfactor_$1.ts 
+/srv/mer/targets/SailfishOS-i486/usr/lib/qt5/bin/lupdate src/qml/{,cover,pages}/*.qml -verbose -ts lang/sailfactor_$1.ts 
+/srv/mer/targets/SailfishOS-i486/usr/lib/qt5/bin/lrelease lang/sailfactor_$1.ts 
