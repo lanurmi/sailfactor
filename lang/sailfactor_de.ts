61e099856208178c22266e06abfccc7f4e4174b8
Lauri Nurmi <lanurmi@iki.fi>
2014-07-06 01:17:03 +0300

    Add German translation.

---
+++ lang/sailfactor_de.ts
@@ -0,0 +1,117 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../src/qml/pages/AboutPage.qml" line="21"/>
+        <source>About Sailfactor %1</source>
+        <translation>Über Sailfactor %1</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/AboutPage.qml" line="25"/>
+        <source>License</source>
+        <translation>Lizenz</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/AboutPage.qml" line="42"/>
+        <source>Author</source>
+        <translation>Autor</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/AboutPage.qml" line="55"/>
+        <source>Source code</source>
+        <translation>Quellcode</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/AboutPage.qml" line="64"/>
+        <source>available at GitHub</source>
+        <translation>auf GitHub verfügbar</translation>
+    </message>
+</context>
+<context>
+    <name>Cover</name>
+    <message>
+        <location filename="../src/qml/cover/Cover.qml" line="23"/>
+        <source>Steps: %1</source>
+        <translation>Schritte: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/cover/Cover.qml" line="37"/>
+        <source>No more steps to carry out.</source>
+        <translation>Keine weitere Schritte um durchzuführen.</translation>
+    </message>
+</context>
+<context>
+    <name>HelpPage</name>
+    <message>
+        <location filename="../src/qml/pages/HelpPage.qml" line="12"/>
+        <source>Sailfactor Help</source>
+        <translation>Hilfe zur Sailfactor</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/HelpPage.qml" line="21"/>
+        <source>Sailfactor is a tool for finding the factors of an integer, step by step. The slider is used to adjust the maximum number of factorization steps to carry out.</source>
+        <translation>Sailfactor ist ein Werkzeug für die Entdeckung von Faktoren einer ganzen Zahl, Schritt für Schritt. Die maximale Anzahl der Faktorisierungs­schritte durchzuführen ist mit dem Schieber einstellbar.</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/HelpPage.qml" line="24"/>
+        <source>&lt;i&gt;Prime&lt;/i&gt; factors are always &lt;b&gt;highlighted&lt;/b&gt; in the output. When the number of steps is high enough, all the factors shown will be prime factors.</source>
+        <translation>&lt;i&gt;Prim&lt;/i&gt;faktoren sind immer &lt;b&gt;markiert&lt;/b&gt; in der Ausgabe. Als die Anzahl der Schritte genügend hoch ist, werden alle gezeigten Faktoren Primfaktoren sein.</translation>
+    </message>
+</context>
+<context>
+    <name>LicensePage</name>
+    <message>
+        <location filename="../src/qml/pages/LicensePage.qml" line="21"/>
+        <source>License for Sailfactor</source>
+        <translation>Lizenz für Sailfactor</translation>
+    </message>
+</context>
+<context>
+    <name>SailFactor</name>
+    <message>
+        <location filename="../src/qml/pages/SailFactor.qml" line="19"/>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/SailFactor.qml" line="26"/>
+        <source>Help</source>
+        <translation>Hilfe</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/SailFactor.qml" line="42"/>
+        <source>Factorization</source>
+        <translation>Faktorisierung</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/SailFactor.qml" line="58"/>
+        <source>Enter an integer.</source>
+        <translation>Eine ganze Zahl eingeben.</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/SailFactor.qml" line="59"/>
+        <source>The integer to factorize.</source>
+        <translation>Die zu faktorisierende ganze Zahl.</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/SailFactor.qml" line="90"/>
+        <source>Steps</source>
+        <translation>Schritte</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/SailFactor.qml" line="90"/>
+        <source>No more steps to carry out</source>
+        <translation>Keine weitere Schritte um durchzuführen</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../src/qml/main.qml" line="22"/>
+        <source>No integer entered yet.</source>
+        <translation>Noch keine Zahl eingegeben.</translation>
+    </message>
+</context>
+</TS>
